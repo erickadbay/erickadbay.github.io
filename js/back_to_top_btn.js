94f1984473579e9290dd7b67d0673ad9080427da
erickadbay <erickadbay@gmail.com>
2016-02-24 04:33:09 -0500

    Initial commit

---
+++ js/back_to_top_btn.js
@@ -0,0 +1,19 @@
+// create the back to top button
+$('footer').prepend('<a href="#" class="back-to-top"><i class="fa fa-arrow-circle-up fa-3"></i></a>');
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+    if ( $(window).scrollTop() > amountScrolled ) {
+        $('a.back-to-top').fadeIn('slow');
+    } else {
+        $('a.back-to-top').fadeOut('slow');
+    }
+});
+
+$('a.back-to-top, a.simple-back-to-top').click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 700);
+    return false;
+});
