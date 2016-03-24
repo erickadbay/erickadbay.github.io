7c7386084b635d03833c794a73f8f1e7a7dd4e22
erickadbay <erickadbay@gmail.com>
2016-03-24 02:25:38 -0400

    testing some animations. In progress...

---
+++ js/switch_functions.js
@@ -0,0 +1,9 @@
+function switchToPrudent(){
+    document.getElementById("prudentcal").style.display = "Block";
+	document.getElementById("get-involved").style.display = "None";
+}
+
+function switchToColorMatch(){
+    document.getElementById("get-involved").style.display = "None";
+    document.getElementById("colormatch").style.display = "Block";
+}
